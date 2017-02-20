f3128d9cb27138945cd5709be6ae353912d9ff9f
benwilcock <benwilcock@gmail.com>
2017-02-20 15:08:47 +0000

    updated

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -14,7 +14,7 @@ if [ $jar_count -gt 1 ]; then
   exit 1
 fi
 
-find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+#find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
 
 echo "Done packaging"
 exit 0
